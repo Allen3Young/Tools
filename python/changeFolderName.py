c3ea5c609088509806b9fac4ecda2fb5301104ff
Yuxuan Yang <yuxuan.yang@oru.se>
2019-11-25 19:31:41 +0100

    change file or folder names by python, update

--- python/changeFolderName.py
+++ python/changeFolderName.py
@@ -4,9 +4,12 @@ import re
 dirs = os.listdir(os.curdir)
 i = 0
 for dir in dirs:
-    
-    if os.path.isdir(dir) == True:
+    # if dir is a folder
+    if os.path.isdir(dir) == True: 
         os.rename(str(dir),str(i))
         i += 1
+
+    # if dir is a file
+    # if os.path.isfile(dir) == True:
 
 #print(dirs)
